935c1535f3d2227857e9b93d7bd3c54c2a3fe6c6
Flysoft <1922656933@qq.com>
2023-06-08 22:32:38 +0800

    refactor(mica): compatibility OS check

--- Mica (Test)/main.js
+++ Mica (Test)/main.js
@@ -1,18 +1,19 @@
 const electron = require("electron");
 const path = require("path");
-const os = require("os");
-const semver = require("semver");
 const { spawnSync } = require("child_process");
 
 const s = path.sep;
 
+const DWM_SYSTEMBACKDROP_TYPE = {
+    DWMSBT_AUTO: 0,
+    DWMSBT_NONE: 1,
+    DWMSBT_MAINWINDOW: 2, // Mica
+    DWMSBT_TRANSIENTWINDOW: 3, // Acrylic
+    DWMSBT_TABBEDWINDOW: 4, // Tabbed
+};
+const flag = DWM_SYSTEMBACKDROP_TYPE.DWMSBT_MAINWINDOW;
+
 module.exports = (qqntim) => {
-    if (process.platform != "win32" || !semver.lte("10.0.22621", os.release())) {
-        console.error(
-            `[Mica] 系统级背景材质不可用。该插件只支持 Windows 11 22621 及以上版本的操作系统。`
-        );
-        return;
-    }
     electron.app.commandLine.appendSwitch("enable-transparent-visuals");
     qqntim.interrupt.windowCreation((args) => {
         return {
@@ -31,12 +32,15 @@ module.exports = (qqntim) => {
     });
     electron.ipcMain.on("___!mica:enable", (event) => {
         const win = electron.BrowserWindow.fromWebContents(event.sender);
-        let handle = win.getNativeWindowHandle().readUint32LE();
-        console.log(`[Mica] 尝试为窗口 ${handle} 设置背景材料。`);
-        spawnSync(`${__dirname}${s}electron_helper.exe`, [
+        const handle = win.getNativeWindowHandle().readUint32LE();
+        console.log(`[Mica] 尝试为窗口 ${handle} 设置背景材料，标志=${flag}`);
+        const helper = spawnSync(`${__dirname}${s}electron_helper.exe`, [
             "set_window_backdrop",
             handle.toString(),
-            (3).toString(),
+            flag.toString(),
         ]);
+        console.log(
+            `[Mica] 设置${helper.status == 0 ? `成功` : `失败`}，状态码=${helper.status}`
+        );
     });
 };
