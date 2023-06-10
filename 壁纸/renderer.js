38d4fba407ee01e24db72ba1ccfc6c3e47a0845e
Flysoft <1922656933@qq.com>
2023-06-10 17:46:26 +0800

    fix(wallpaper): image cannot be loaded on Windows

--- 壁纸/renderer.js
+++ 壁纸/renderer.js
@@ -1,11 +1,14 @@
+const path = require("path");
+
+const s = path.sep;
+
 module.exports = (qqntim) => {
     qqntim.windowLoadPromise.then(() => {
-        let wallpaperURL = process.env["QQ_WALLPAPER_FILE"];
-        if (wallpaperURL) {
+        const wallpaperFile = process.env["QQ_WALLPAPER_FILE"];
+        if (wallpaperFile) {
+            const wallpaperURL = encodeURI(`file://${wallpaperFile.replaceAll(s, "/")}`);
             console.log(`[Wallpaper] 加载壁纸： ${wallpaperURL}`);
-            document.body.style.backgroundImage = `url(${encodeURI(
-                `file://${wallpaperURL}`
-            )})`;
+            document.body.style.backgroundImage = `url(${wallpaperURL})`;
         }
     });
 };
