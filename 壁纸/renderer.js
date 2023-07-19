c45778be8e4512d1e855cb647c3d85de948d463d
Hi Flysoft <flysoftbeta@outlook.com>
2023-07-19 23:41:13 +0800

    fix(wallpaper): bug fixes

--- 壁纸/renderer.js
+++ 壁纸/renderer.js
@@ -2,12 +2,18 @@ const path = require("path");
 const s = path.sep;
 
 module.exports.default = class Entry {
-    onWindowLoaded() {
-        const wallpaperFile = process.env["QQ_WALLPAPER_FILE"];
-        if (wallpaperFile) {
-            const wallpaperURL = encodeURI(`file://${wallpaperFile.replaceAll(s, "/")}`);
-            console.log(`[Wallpaper] 加载壁纸： ${wallpaperURL}`);
-            document.body.style.backgroundImage = `url(${wallpaperURL})`;
-        }
-    }
-}
+	onWindowLoaded() {
+		const wallpaperFile = process.env["QQ_WALLPAPER_FILE"];
+		if (wallpaperFile) {
+			const wallpaperURL = encodeURI(
+				`file://${wallpaperFile.replaceAll(s, "/")}`,
+			);
+			console.log(`[Wallpaper] 加载壁纸： ${wallpaperURL}`);
+			document.body.style.setProperty(
+				"background-image",
+				`url(${wallpaperURL})`,
+				"important",
+			);
+		}
+	}
+};
