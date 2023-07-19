c888e80796d5d807606fe312a551077415327324
Meow Nova <n@ova.moe>
2023-07-19 09:49:08 +0800

    feat(wallpaper): make it compatible with QQNTim 3.0 (#6)
    * Make Wallpaper compatible with QQNTim3.0
    
    * Update renderer.js
    
    ---------
    
    Co-authored-by: Flysoft <flysoftbeta@outlook.com>

--- 壁纸/renderer.js
+++ 壁纸/renderer.js
@@ -1,14 +1,13 @@
 const path = require("path");
-
 const s = path.sep;
 
-module.exports = (qqntim) => {
-    qqntim.windowLoadPromise.then(() => {
+module.exports.default = class Entry {
+    onWindowLoaded() {
         const wallpaperFile = process.env["QQ_WALLPAPER_FILE"];
         if (wallpaperFile) {
             const wallpaperURL = encodeURI(`file://${wallpaperFile.replaceAll(s, "/")}`);
             console.log(`[Wallpaper] 加载壁纸： ${wallpaperURL}`);
             document.body.style.backgroundImage = `url(${wallpaperURL})`;
         }
-    });
-};
+    }
+}
